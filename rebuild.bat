73e6c7c09c27ab3c557e27465a6ac93918ca1c69
peTheProgrammer <peTheProgrammer@gmail.com>
2022-03-19 17:22:14 +0100

    Added rebuild script

---
+++ rebuild.bat
@@ -0,0 +1,21 @@
+@echo off
+
+CALL _util\_init.bat
+
+if %errorlevel% NEQ 0 (
+	EXIT /b 1
+)
+
+CALL _util\premake_execute.bat
+
+setlocal enabledelayedexpansion
+
+set method=rebuild
+if NOT [%1]==[] (
+	set target=%1
+)
+CALL _util\msbuild_execute.bat
+
+endlocal
+
+EXIT /b 0
